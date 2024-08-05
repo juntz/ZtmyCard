328e10e3249f997b3f2a37b333234afa73f3b04c
ffdd270.macbook <ffdd270@gmail.com>
2024-08-06 00:59:33 +0900

    impl : implement 2 card rule. add global warning UI.

---
+++ scripts/global_ui.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+@onready var warning_scene := preload("res://warning.tscn")
+var _global_root_ui : CanvasLayer = null
+
+func send_warning(message: String):
+	var warning = warning_scene.instantiate()
+	warning.get_node("Label").text = message
+	_global_root_ui.add_child(warning)
+
+	warning.position.y = -200
+	var tween = get_tree().create_tween()
+	tween.tween_property(warning, "position:y", 100, 0.5)
+	tween.tween_interval(1.0)
+	tween.tween_property(warning, "modulate:a", 0, 0.5)
+	tween.tween_callback(warning.queue_free)
+
+	return warning
+
+func _ready():
+	var root = get_tree().root
+	_global_root_ui = CanvasLayer.new()
+	root.add_child.call_deferred(_global_root_ui)
